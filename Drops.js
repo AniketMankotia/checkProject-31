170b9b25a056e8b03a5e243d83b44bc5b6e7a71f
AniketMankotia <75171100+AniketMankotia@users.noreply.github.com>
2021-02-09 13:46:07 +0530

    Update Drops.js

--- Drops.js
+++ Drops.js
@@ -4,11 +4,9 @@ class Drops extends BaseClass{
     }
     display(){
         super.display();
-        //if(frameCount % 200 === 0){
             //for(var i=0; i<maxDrops; i++){
                 //drops.push(new Drops(random(0,400), random(0,400)));
             //}
-        //}
         if(this.body.position.y > height){
             Matter.Body.setPosition(this.body, {x:random(0,400), y:random(0,400)});
         }
